839d6df9f39a41f1d3acb847b16904602cff00ac
El Mehdi ASSALI <assalielmehdi@gmail.com>
2021-06-15 02:14:16 +0200

    :sparkles: Call gRPC server from REST API

--- rest-api/index.js
+++ rest-api/index.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
 
+const PROTO_PATH = './proto/greeter.proto';
+const protoLoader = require('@grpc/proto-loader');
+const packageDefinition = protoLoader.loadSync(PROTO_PATH);
+
+const grpc = require('@grpc/grpc-js');
+const greetProto = grpc.loadPackageDefinition(packageDefinition).greeter;
+
+const GRPC_TARGET = 'localhost:8081';
+
+const greet = (name, httpRes) => {
+  const client = new greetProto.Greeter(GRPC_TARGET, grpc.credentials.createInsecure());
+
+  client.greet({name}, (err, res) => {
+    if (!err) {
+      httpRes.json(res.greeting);
+    }
+  });
+};
+
 app.get('/greet', (req, res) => {
   console.log('[REST API] New request on /greet');
 
   const name = req.query.name || 'World';
-  res.json(`Hello ${name}`);
+  greet(name, res);
 });
 
 const PORT = process.env.PORT || 8080;
